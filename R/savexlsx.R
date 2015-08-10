d3f5a388c72362bfb247c795fe04d6e6d9f9a5ea
Michael Sousa <mike.psousa@gmail.com>
2015-08-10 18:23:10 -0400

    added savxlsx.R function to reco

---
+++ R/savexlsx.R
@@ -0,0 +1,23 @@
+#' Writes R objects to multiple excel sheets in a single excel file
+#'
+#' @param string file name of excel file to be written.
+#' @param object name of R object(s).
+#' @return Written file.
+
+#convert variable names to underscore format
+
+save.xlsx <- function (file, ...)
+{
+    require(xlsx, quietly = TRUE)
+    objects <- list(...)
+    fargs <- as.list(match.call(expand.dots = TRUE))
+    objnames <- as.character(fargs)[-c(1, 2)]
+    nobjects <- length(objects)
+    for (i in 1:nobjects) {
+        if (i == 1)
+            write.xlsx(objects[[i]], file, sheetName = objnames[i])
+        else write.xlsx(objects[[i]], file, sheetName = objnames[i],
+                        append = TRUE)
+    }
+    print(paste("Workbook", file, "has", nobjects, "worksheets."))
+}
